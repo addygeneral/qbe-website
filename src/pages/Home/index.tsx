7d3a85e1fe6a215a4dc697d3e881d02b360b21a8
unknown <sammcaddy@gmail.com>
2024-11-24 11:43:35 +0000

    Initial commit

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,112 @@
+import { lazy } from "react";
+import IntroContent from "../../content/IntroContent.json";
+//import MiddleBlockContent from "../../content/MiddleBlockContent.json";
+import AboutContent from "../../content/AboutContent.json";
+import MissionContent from "../../content/MissionContent.json";
+import ProductContent from "../../content/ProductContent.json";
+import TenioContent from "../../content/TenioContent.json";
+import ContactContent from "../../content/ContactContent.json";
+import DigitalContent from "../../content/DigitalContent.json";
+import Keenvisioncontent from "../../content/Keenvisioncontent.json";
+import UnlockContent from "../../content/UnlockContent.json";
+
+
+const Contact = lazy(() => import("../../components/ContactForm"));
+//const MiddleBlock = lazy(() => import("../../components/MiddleBlock"));
+const Container = lazy(() => import("../../common/Container"));
+const ScrollToTop = lazy(() => import("../../common/ScrollToTop"));
+const ContentBlock = lazy(() => import("../../components/ContentBlock"));
+
+const Home = () => {
+  return (
+    <Container>
+      <ScrollToTop />
+      <ContentBlock
+        direction="right"
+        title={IntroContent.title}
+        content={IntroContent.text}
+        button={IntroContent.button}
+        icon="developer.svg"
+        id="intro"
+      />
+      <ContentBlock
+        direction="left"
+        title={ProductContent.title}
+        content={ProductContent.text}
+        button={ProductContent.button}
+        icon="waving.svg"
+        id="product"
+      />
+      <ContentBlock
+        direction="left"
+        title={AboutContent.title}
+        content={AboutContent.text}
+        section={AboutContent.section}
+        icon="graphs.svg"
+        id="about"
+      />
+      <ContentBlock
+        direction="left"
+        title={TenioContent.title}
+        content={TenioContent.text}
+        button={TenioContent.button}
+        icon="ourbrands.svg"
+        id="product"
+      />
+      <ContentBlock
+        direction="right"
+        title={MissionContent.title}
+        content={MissionContent.text}
+        icon="qbeevents.svg"
+        id="mission"
+      />
+      <ContentBlock
+        direction="left"
+        title={DigitalContent.title}
+        content={DigitalContent.text}
+        icon="dds.svg"
+        id="mission"
+      />
+      <ContentBlock
+        direction="right"
+        title={Keenvisioncontent.title}
+        content={Keenvisioncontent.text}
+        icon="keenvision.svg"
+        id="mission"
+      />
+      <ContentBlock
+        direction="center" 
+        title=""
+        content=""
+        icon="Rectangle102.jpg"
+        id="image-block"
+      />
+      {/*
+      <div className="content-block image-container">
+        <img src={RectangleImage} alt="Rectangle" />
+      </div>
+
+       <MiddleBlock
+        title={MiddleBlockContent.title}
+        content={MiddleBlockContent.text}
+        button={MiddleBlockContent.button}
+      />
+      */}
+      <ContentBlock
+        direction="left"
+        title={UnlockContent.title}
+        content={UnlockContent.text}
+        icon="unlock.svg"
+        id="mission"
+      />
+      
+      <Contact
+        title={ContactContent.title}
+        content={ContactContent.text}
+        id="contact"
+      />
+    </Container>
+  );
+};
+
+export default Home;
